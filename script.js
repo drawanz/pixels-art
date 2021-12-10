534281deb42fddd7904bb85d10f730ae680c8cbf
Leonidas <leonidas.drawanz@gmail.com>
2021-12-10 19:30:39 -0300

    exercicios 4, 5 6 e 7

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-
 // organizando os paletes lado a lado e colocando cor neles
 let elementoPai = document.getElementById("color-palette");
 let pallete = document.getElementsByClassName("color");
-let arrayCores = ['Black', 'blue', 'red', 'orange']
-for ( let i = 0; i < pallete.length; i +=1){
-    elementoPai.appendChild(pallete[i]);
-    pallete[i].style.position = "absolute";
-    pallete[i].style.top = '152px';
-    pallete[i].style.left = (8 + 55*[i]) + 'px';
-    pallete[i].style.backgroundColor = arrayCores[i]
-}
+let arrayCores = ['Black', 'blue', 'red', 'orange'];
+
+
+    function criandoPalletes (parametro) {
+        for ( let i = 0; i < parametro; i +=1){
+            elementoPai.appendChild(pallete[i]);
+            pallete[i].style.position = "absolute";
+            pallete[i].style.top = '152px';
+            pallete[i].style.left = (8 + 54*[i]) + 'px';
+            pallete[i].style.backgroundColor = arrayCores[i]
+        }
+    }   
+
+     
+
+// criando as divisões para criar o "pixelboard"
+// esse treicho de código foi inspirado após uma conversa com o colega Fernando Rocha, da tribo C, o qual conversou comigo em uma dinâmica do zoom no dia 09/12/21, sobre usar divs dentro de divs, ao invés de criar e alocar div por div individualmente.  
+    let sectionPixelBoard = document.createElement("section");
+    document.body.appendChild(sectionPixelBoard);
+    sectionPixelBoard.setAttribute("id", "pixel-board");
+    sectionPixelBoard.style.position = "absolute";
+    sectionPixelBoard.style.top = "220px";
+
+    
+    function quantidadePixels (quantidade) {
+        for ( let i = 1; i <= quantidade; i +=1){
+            let div = document.createElement("div");
+            sectionPixelBoard.appendChild(div);
+            div.className = "pixel"
+            div.style.backgroundColor= "white";
+        }
+    }
+
+
+    // resolução do requisito 6
+    
+   
+    function pintandoPixels (){
+        pallete[0].classList.add("selected");
+    }
+
+    
+   
+    
+    window.onload = function () {
+        criandoPalletes (pallete.length);  
+        quantidadePixels(25);
+        pintandoPixels();
+    }
 
     /* let pallete0 = document.getElementsByClassName("color")[0];
     pallete0.style.position = "absolute";
@@ -27,3 +67,4 @@ for ( let i = 0; i < pallete.length; i +=1){
     let pallete3 = document.getElementsByClassName("color")[3];
     pallete3.style.position = "absolute";
     pallete3.style.top = "300px"*/ 
+
