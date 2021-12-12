c0599da9cb3af2d371ac40b2e60a2690b0a87aa5
Leonidas <leonidas.drawanz@gmail.com>
2021-12-12 17:56:12 -0300

    7 e 8 feitos, 6 com um probleminha

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // organizando os paletes lado a lado e colocando cor neles
-let elementoPai = document.getElementById("color-palette");
-let pallete = document.getElementsByClassName("color");
-let arrayCores = ['Black', 'blue', 'red', 'orange'];
+    let elementoPai = document.getElementById("color-palette");
+    let pallete = document.getElementsByClassName("color");
+    let arrayCores = ['black', 'blue', 'red', 'orange'];
 
 
     function criandoPalletes (parametro) {
@@ -14,7 +14,7 @@ let arrayCores = ['Black', 'blue', 'red', 'orange'];
         }
     }   
 
-     
+    criandoPalletes (pallete.length);       
 
 // criando as divisões para criar o "pixelboard"
 // esse treicho de código foi inspirado após uma conversa com o colega Fernando Rocha, da tribo C, o qual conversou comigo em uma dinâmica do zoom no dia 09/12/21, sobre usar divs dentro de divs, ao invés de criar e alocar div por div individualmente.  
@@ -34,22 +34,88 @@ let arrayCores = ['Black', 'blue', 'red', 'orange'];
         }
     }
 
+    quantidadePixels(25);
 
-    // resolução do requisito 6
-    
-   
-    function pintandoPixels (){
-        pallete[0].classList.add("selected");
+
+    // resolução do requisito 6 e resolução requisito 7
+    let pintandoDePreto = pallete[0];
+    pintandoDePreto.classList.add("selected"); 
+    let sectionPaiPixelBoard = document.getElementById("pixel-board");
+
+    function removendoClasseSelected () {     
+        let abc = document.querySelector('.selected')  
+        abc.classList.remove('selected')     
+        /*for (let i=0; i < arrayCores.length; i +=1){
+            pallete[i].classList.remove("selected");
+        }*/
+    }        
+
+    function vamosPintar () {        
+        elementoPai.addEventListener('click', function(event){
+            let corAplicavel = event.target.style.backgroundColor;     
+            removendoClasseSelected();
+            event.target.classList.add("selected")
+            
+            sectionPaiPixelBoard.addEventListener('click', function(event){             
+                event.target.style.backgroundColor = corAplicavel;
+            })
+        }) 
     }
 
+    vamosPintar();
+
+
+
+    //let selecionandoCores = document.querySelectorAll('.color')
+    
+    
+    /* function removendoClasseSelected () {
+        for (let i=0; i < arrayCores.length; i +=1){
+            pallete[i].classList.remove("selected");
+        }
+    }
+
+    function selecionandoCor () {
+        pallete[0].addEventListener('click', function(event){
+            removendoClasseSelected ();
+            pallete[0].classList.add("selected");
+            let classSelected = document.querySelector(".selected"); //pego o primeiro(e único) elemento que possui a classe selected e vou usar isso para alterar o backgroundColor.
+            classSelected.style.backgroundColor = arrayCores[0];   
+        })
+
+        let pintandoPixel = document.getElementsByClassName("pixel");
+        console.log(pintandoPixel);*/ 
+
+       /* pintandoPixel.addEventListener('click', function(){
+            pintandoPixel.className = "selected"
+        })*/
+
+
+
+        /* selecionandoCores[1].addEventListener('click', function(event){
+            console.log(arrayCores[1]);
+        })
+        selecionandoCores[2].addEventListener('click', function(event){
+            console.log(arrayCores[2]);
+        })
+        selecionandoCores[3].addEventListener('click', function(event){
+            console.log(arrayCores[3]);
+        }) */
+    
+
+
+
     
    
+    // window onload para finalizar os requisitos
+
     
-    window.onload = function () {
-        criandoPalletes (pallete.length);  
-        quantidadePixels(25);
-        pintandoPixels();
-    }
+    
+
+    /* window.onload = function () {
+        
+        //vamosPintar();
+    } */ 
 
     /* let pallete0 = document.getElementsByClassName("color")[0];
     pallete0.style.position = "absolute";
