0d53bf4bcf4cd9feae64859f6007a00e2758ad5d
Leonidas <leonidas.drawanz@gmail.com>
2022-02-10 17:37:36 -0300

    removendo erros do lint

--- script.js
+++ script.js
@@ -1,111 +1,122 @@
+const coresPaletta = document.querySelectorAll('.color');
+const arrayCores = ['black'];
+const pixelBoard = document.getElementById('pixel-board');
+const divPixel = document.getElementById('pixel-board');
+const paletta = document.getElementById('color-palette');
+const valorNxN = document.getElementById('board-size');
+const button = document.getElementById('clear-board');
+const vqv = document.getElementById('generate-board');
+const valorInput = valorNxN.value;
 
-let coresPaletta = document.querySelectorAll(".color");
-let arrayCores = ["black"];
-let pixelBoard = document.getElementById("pixel-board");
-let divPixel = document.getElementById("pixel-board");
-let paletta = document.getElementById("color-palette");
-let valorNxN = document.getElementById("board-size"); 
-let button = document.getElementById("clear-board");
-let vqv = document.getElementById("generate-board");
-
-//criar as palettas e dar cor 
+//  criar as palettas e dar cor
 function criandoPalettas() {
-    for ( let i =0; i < arrayCores.length; i += 1){
-        coresPaletta[i].style.backgroundColor = arrayCores[i];
-    }
+  for (let i = 0; i < arrayCores.length; i += 1) {
+    coresPaletta[i].style.backgroundColor = arrayCores[i];
+  }
 }
 
-//criando cores aleatórias COMEÇANDO AQUI
-function criandoCoresAleatórias (){
-    for (let i=0; i< 3; i+=1){
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        let corAleatoria = "rgb(" + r + "," + g + "," + b + ")";
-        arrayCores.push(corAleatoria);   
-    }
+// criando cores aleatórias COMEÇANDO AQUI
+function criandoCoresAleatórias() {
+  for (let i = 0; i < 3; i += 1) {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const corAleatoria = `rgb(${r}, ${g}, ${b})`;
+    arrayCores.push(corAleatoria);
+  }
 }
 criandoCoresAleatórias();
 
-//criando palettas
-// esse treicho de código onde eu aplico um laço for para criar as divs, foi inspirado após uma conversa com o colega Fernando Rocha, da tribo C, o qual conversou comigo sobre usar divs dentro de divs, invés de criar e alocar div por div individualmente
-function criandoPixels (quantidadePixels){  
-    pixelBoard.style.width = (42*(Math.pow(quantidadePixels, 0.5)))+'px'; //math.pow calcula a raiz do número Math.pow(base, expoente)
-    for (let i =0; i < quantidadePixels; i +=1){
-        let divPixel = document.createElement("div")
-        pixelBoard.appendChild(divPixel);
-        divPixel.className = "pixel"
-        divPixel.style.backgroundColor = "white";
-    }
+//  criando palettas
+function criandoPixels(quantidadePixels) {
+  const widthValor = (42 * (quantidadePixels ** 0.5));
+  pixelBoard.style.width = `${widthValor}px`;
+  // math.pow calcula a raiz do número Math.pow(base, expoente)
+  for (let i = 0; i < quantidadePixels; i += 1) {
+    const divPixels = document.createElement('div');
+    pixelBoard.appendChild(divPixels);
+    divPixels.className = 'pixel';
+    divPixels.style.backgroundColor = 'white';
+  }
 }
 criandoPixels(25);
 
-//fazer iniciar com a cor preta 
 function iniciandoCorPreta() {
-    coresPaletta[0].classList.add("selected");   
+  coresPaletta[0].classList.add('selected');
 }
 iniciandoCorPreta();
 
-function pintandoDePreto(event){        
-    event.target.style.backgroundColor = "black";
+function pintandoDePreto(event) {
+  // eslint-disable-next-line no-param-reassign
+  event.target.style.backgroundColor = 'black';
 }
 
-//remove a classe selected de todos lugares, seleciona a cor e depois aplica nos pixels
-function selecionaCor(event){
-    for (let i=0; i < arrayCores.length; i +=1){
-        coresPaletta[i].classList.remove("selected");
-    }    
-    let corAplicavel = event.target.style.backgroundColor;
-        event.target.classList.add("selected");
-    divPixel.addEventListener('click', function (event) {
-        event.target.style.backgroundColor = corAplicavel;
-    })
+function selecionaCorEAplica(event) {
+  for (let i = 0; i < arrayCores.length; i += 1) {
+    coresPaletta[i].classList.remove('selected');
+  }
+  const corAplicavel = event.target.style.backgroundColor;
+  event.target.classList.add('selected');
+  divPixel.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = corAplicavel;
+  });
 }
 
-//bloco de código para limpar as cores  do quadro
+const backgroundColor = () => {
+  const todasDivsPixelBoard = document.querySelectorAll('.pixel');
+  for (let i = 0; i < 25; i += 1) {
+    todasDivsPixelBoard[i].style.backgroundColor = 'white';
+  }
+};
+
+const backgroundColor5 = () => {
+  const todasDivsPixelBoard = document.querySelectorAll('.pixel');
+  for (let i = 0; i < valorInput * valorInput; i += 1) {
+    todasDivsPixelBoard[i].style.backgroundColor = 'white';
+  }
+};
+
+function removeDivs() {
+  while (divPixel.firstChild) {
+    divPixel.removeChild(divPixel.firstChild);
+  }
+}
+
 function limparCores() {
-    let valorInput = valorNxN.value; 
-    let todasDivsPixelBoard = document.querySelectorAll('.pixel');
-    if (valorInput > 5){
-        for ( let i = 0; i < valorInput*valorInput; i +=1){
-            todasDivsPixelBoard[i].style.backgroundColor = "white";
-        }
-    } 
-    else {
-        for ( let i = 0; i < 25; i +=1){
-            todasDivsPixelBoard[i].style.backgroundColor = "white";
-        }
-    }
+  if (valorInput > 5) {
+    backgroundColor5();
+  } else {
+    backgroundColor();
+  }
+  removeDivs();
+  criandoPixels(25);
 }
 
-//bloco de código para apagar todas as divs e gerar novo board usando o botão VQV    
+//  bloco de código para apagar todas as divs e gerar novo board usando o botão VQV
 function aumentarPixelBoard() {
-    let valorInput = valorNxN.value;
-    while (divPixel.firstChild){ //enquanto houver firstChilds, irá remover sempre o firstChild
-        divPixel.removeChild(divPixel.firstChild)
-    }
-    if (valorInput > 0 && valorInput < 5){ //requisito 11
-        //colegas da turma 19-C, Hellen Ribas, Emerson Alves e Diogo Martini me ajudaram a fazer essa lógica das condicionais            
-        valorInput = 5;
-        criandoPixels (valorInput*valorInput);
-    }
-    else if (valorInput >= 5 && valorInput <= 50){
-        criandoPixels (valorInput*valorInput);
-    }
-    else if (valorInput > 50){
-        valorInput = 50;
-        criandoPixels (valorInput*valorInput);
-    }
-    else {
-        alert("Board inválido!");
-    }
+  let valorInput = valorNxN.value;
+  removeDivs();
+  if (valorInput > 0 && valorInput < 5) {
+    valorInput = 5;
+    criandoPixels (valorInput*valorInput);
+  }
+  else if (valorInput >= 5 && valorInput <= 50) {
+    criandoPixels (valorInput*valorInput);
+  }
+  else if (valorInput > 50) {
+    valorInput = 50;
+    criandoPixels (valorInput*valorInput);
+  }
+  else {
+    alert("Board inválido!");
+  }
 }
 
 divPixel.addEventListener('click', pintandoDePreto);
-paletta.addEventListener('click', selecionaCor);
+paletta.addEventListener('click', selecionaCorEAplica);
 button.addEventListener('click', limparCores);
 vqv.addEventListener('click', aumentarPixelBoard);
 
 window.onload = function () {
-    criandoPalettas();   
-}
+  criandoPalettas();
+};
